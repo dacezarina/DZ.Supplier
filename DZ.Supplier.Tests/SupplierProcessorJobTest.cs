10c73ec04ab52bb498e0fdfdb53d68a30579f46a
dacezarina <zarina.dace@yahoo.com>
2025-06-07 21:20:59 +0300

    Added test for background job

---
+++ DZ.Supplier.Tests/SupplierProcessorJobTest.cs
@@ -0,0 +1,44 @@
+﻿using DZ.SupplierProcessor;
+using Microsoft.Extensions.Logging;
+using Moq;
+
+namespace DZ.Supplier.Tests
+{
+    [TestFixture]
+    public class SupplierProcessorJobTest
+    {
+        [Test]
+        public async Task RunTest()
+        {
+            var loggerMock = new Mock<ILogger<SupplierProcessorJob>>(MockBehavior.Strict);
+
+            loggerMock.Setup(x => x.Log(
+                LogLevel.Information,
+                It.IsAny<EventId>(),
+                It.IsAny<object>(),
+                It.IsAny<Exception>(),
+                It.IsAny<Func<object, Exception?, string>>()
+            ));
+
+            loggerMock.Setup(x => x.Log(
+                LogLevel.Information,
+                It.IsAny<EventId>(),
+                It.IsAny<It.IsAnyType>(),
+                null,
+                It.IsAny<Func<It.IsAnyType, Exception?, string>>()
+            ));
+
+            loggerMock.Setup(x => x.Log(
+                LogLevel.Information,
+                It.IsAny<EventId>(),
+                It.IsAny<It.IsAnyType>(),
+                null,
+                It.IsAny<Func<It.IsAnyType, Exception?, string>>()
+            ));
+
+            var supplierProcessorJob = new SupplierProcessorJob(loggerMock.Object);
+
+            await supplierProcessorJob.Run();
+        }
+    }
+}
