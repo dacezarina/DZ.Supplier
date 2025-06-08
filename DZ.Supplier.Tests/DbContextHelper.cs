cd98758ea51788a7267da4b9f7e659c369d53c48
dacezarina <zarina.dace@yahoo.com>
2025-06-08 15:07:37 +0300

    Added in memory database for testing purposes

---
+++ DZ.Supplier.Tests/DbContextHelper.cs
@@ -0,0 +1,17 @@
+﻿using DZ.SupplierProcessor.Database;
+using Microsoft.EntityFrameworkCore;
+
+namespace DZ.SupplierProcessor.Tests
+{
+    public static class DbContextHelper
+    {
+        public static SupplierDbContext CreateContext()
+        {
+            var _contextOptions = new DbContextOptionsBuilder<SupplierDbContext>()
+                .UseInMemoryDatabase(databaseName: "in_memory_supplier_database")
+                .Options;
+
+            return new SupplierDbContext(_contextOptions);
+        }
+    }
+}
