ed94dbef139d0a77865d7394750ab6a2746db8ab
dacezarina <zarina.dace@yahoo.com>
2025-06-13 17:24:56 +0300

    Added mappers from DTO to database classes

---
+++ DZ.Supplier/Database/Mapping/BoxMapper.cs
@@ -0,0 +1,22 @@
+﻿using DZ.SupplierProcessor.Database.Models;
+using DZ.SupplierProcessor.Dto;
+
+namespace DZ.SupplierProcessor.Database.Mapping
+{
+    // Can be used some external library for mapping but as project is small 
+    // didn't any library for that
+    public static class BoxMapper
+    {
+        public static Box MapToBox(BoxDto dto)
+        {
+            return new Box()
+            {
+                SupplierIdentifier = dto.SupplierIdentifier,
+                BoxIdentifier = dto.BoxIdentifier,
+                CreatedBy = "SUPPLIER_PROCESSOR",
+                CreatedDate = DateTime.UtcNow,
+                Contents = dto.Products.Select(ContentMapper.MapToContent).ToList()
+            };
+        }
+    }
+}
---
+++ DZ.Supplier/Database/Mapping/ContentMapper.cs
@@ -0,0 +1,24 @@
+﻿using DZ.SupplierProcessor.Database.Models;
+using DZ.SupplierProcessor.Dto;
+
+namespace DZ.SupplierProcessor.Database.Mapping
+{
+    // Can be used some external library for mapping but as project is small 
+    // didn't any library for that
+    public static class ContentMapper
+    {
+        public static Content MapToContent(ProductDto dto)
+        {
+            return new Content()
+            {
+                PoNumber =  dto.PoNumber,
+                Isbn = dto.ISBN,
+                // this is not save tryparse should be used instead, might fail 
+                // as there could be mismatch in types
+                Quantity = int.Parse(dto.Quantity),
+                CreatedBy = "SUPPLIER_PROCESSOR",
+                CreatedDate = DateTime.UtcNow
+            };
+        }
+    }
+}
