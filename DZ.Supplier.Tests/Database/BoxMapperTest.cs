2489abb78cad54eb73c08609d1d5fb14ad2c390f
dacezarina <zarina.dace@yahoo.com>
2025-06-13 17:48:40 +0300

    Added tests for Mappers

---
+++ DZ.Supplier.Tests/Database/BoxMapperTest.cs
@@ -0,0 +1,53 @@
+﻿using DZ.SupplierProcessor.Database.Mapping;
+using DZ.SupplierProcessor.Dto;
+
+namespace DZ.SupplierProcessor.Tests.Database
+{
+    [TestFixture]
+    public class BoxMapperTest
+    {
+        [Test]
+        public void MapToBox_MapsBasicProperties_ReturnsBox()
+        {
+            var dto = new BoxDto(
+                supplierIdentifier: "TRSP117",
+                boxIdentifier: "6874454I");
+
+            var result = BoxMapper.MapToBox(dto);
+
+            Assert.That(result.SupplierIdentifier, Is.EqualTo(dto.SupplierIdentifier));
+            Assert.That(result.BoxIdentifier, Is.EqualTo(dto.BoxIdentifier));
+            Assert.That(result.Contents, Is.Not.Null);
+            Assert.That(result.Contents, Is.Empty);
+            Assert.That(result.CreatedBy, Is.EqualTo("SUPPLIER_PROCESSOR"));
+        }
+
+        [Test]
+        public void MapToBox_InputBoxWithProducts_ReturnsBox()
+        {
+            var dto = new BoxDto(
+                supplierIdentifier: "TRSP117",
+                boxIdentifier: "6874454I");
+
+            dto.Products.AddRange(
+                new ProductDto("P000001661", "9781465121550", "12"),
+                new ProductDto("P000001662", "9781465121550", "12"));
+
+            var result = BoxMapper.MapToBox(dto);
+
+            Assert.That(result.SupplierIdentifier, Is.EqualTo(dto.SupplierIdentifier));
+            Assert.That(result.BoxIdentifier, Is.EqualTo(dto.BoxIdentifier));
+            Assert.That(result.Contents, Has.Count.EqualTo(2));
+            Assert.That(result.Contents.First().PoNumber, Is.EqualTo("P000001661"));
+            Assert.That(result.Contents.First().Quantity, Is.EqualTo(12));
+            Assert.That(result.Contents.Last().PoNumber, Is.EqualTo("P000001662"));
+            Assert.That(result.Contents.Last().Quantity, Is.EqualTo(12));
+        }
+
+        [Test]
+        public void MapToBox_NullObjectInput_ReturnException()
+        {
+            Assert.Throws<NullReferenceException>(() => BoxMapper.MapToBox(null));
+        }
+    }
+}
---
+++ DZ.Supplier.Tests/Database/ContentMapperTest.cs
@@ -0,0 +1,61 @@
+﻿using DZ.SupplierProcessor.Database.Mapping;
+using DZ.SupplierProcessor.Dto;
+
+namespace DZ.SupplierProcessor.Tests.Database
+{
+    [TestFixture]
+    public class ContentMapperTest
+    {
+        [Test]
+        public void MapToContent_ReturnsValidContent()
+        {
+            var dto = new ProductDto("G000009810", "9781473662179", "12");
+
+            var result = ContentMapper.MapToContent(dto);
+
+            Assert.That(result.PoNumber, Is.EqualTo(dto.PoNumber));
+            Assert.That(result.Isbn, Is.EqualTo(dto.ISBN));
+            Assert.That(result.Quantity, Is.EqualTo(12));
+            Assert.That(result.CreatedBy, Is.EqualTo("SUPPLIER_PROCESSOR"));
+        }
+
+        [Test]
+        public void MapToContent_WhenIsbnIsNull_ReturnsValidContent()
+        {
+            var dto = new ProductDto("G000009810", null, "12");
+
+            var result = ContentMapper.MapToContent(dto);
+
+            Assert.That(result.PoNumber, Is.EqualTo(dto.PoNumber));
+            Assert.That(result.Isbn, Is.EqualTo(dto.ISBN));
+            Assert.That(result.Quantity, Is.EqualTo(12));
+            Assert.That(result.CreatedBy, Is.EqualTo("SUPPLIER_PROCESSOR"));
+        }
+
+        [Test]
+        public void MapToContent_NotValidQuantity_ReturnsFormatException()
+        {
+            var dto = new ProductDto("G000009810", "9781473662179", "not_a_number");
+
+            Assert.Throws<FormatException>(() => ContentMapper.MapToContent(dto));
+        }
+
+        [Test]
+        public void MapToContent_InputQuantityTooLarge_ReturnsOverFlowException()
+        {
+            var dto = new ProductDto("G000009810", "9781473662179", "99999999999999999999");
+
+            Assert.Throws<OverflowException>(() => ContentMapper.MapToContent(dto));
+        }
+
+        [Test]
+        public void MapToContent_InputQuantityWithSpaces_ReturnsValidContent()
+        {
+            var dto = new ProductDto("G000009810", "9781473662179", "  15  ");
+
+            var result = ContentMapper.MapToContent(dto);
+
+            Assert.That(15, Is.EqualTo(result.Quantity));
+        }
+    }
+}
