fda71ad9f4ee23b4fbea9dd5b4ad8abb296fd62e
dacezarina <zarina.dace@yahoo.com>
2025-06-13 17:23:15 +0300

    Added Box table

--- DZ.Supplier/Database/Migrations/20250611173441_AddedBoxTable.Designer.cs
+++ DZ.Supplier/Database/Migrations/20250611173441_AddedBoxTable.Designer.cs
@@ -4,6 +4,7 @@ using DZ.SupplierProcessor.Database;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace DZ.SupplierProcessor.Database.Migrations
 {
     [DbContext(typeof(SupplierDbContext))]
-    partial class SupplierDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250611173441_AddedBoxTable")]
+    partial class AddedBoxTable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ DZ.Supplier/Database/Migrations/20250611173441_AddedBoxTable.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DZ.SupplierProcessor.Database.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedBoxTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "BoxId",
+                table: "Contents",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateTable(
+                name: "Boxes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    SupplierIdentifier = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    BoxIdentifier = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CreatedDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    CreatedBy = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    UpdatedDate = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    UpdatedBy = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Boxes", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Contents_BoxId",
+                table: "Contents",
+                column: "BoxId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Contents_Boxes_BoxId",
+                table: "Contents",
+                column: "BoxId",
+                principalTable: "Boxes",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Contents_Boxes_BoxId",
+                table: "Contents");
+
+            migrationBuilder.DropTable(
+                name: "Boxes");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Contents_BoxId",
+                table: "Contents");
+
+            migrationBuilder.DropColumn(
+                name: "BoxId",
+                table: "Contents");
+        }
+    }
+}
